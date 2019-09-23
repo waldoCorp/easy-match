beab751d75da05cd0e4b471589cf961b63fdca5e
Lief <williamlief@gmail.com>
2019-09-23 18:12:12 -0500

    moved link bar to header file

---
+++ website/header.php
@@ -0,0 +1,20 @@
+<!-- Link Bar -->
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <a href="./show_names.php">Show Me Names!</a>
+    </div>
+
+    <div class="col-sm">
+      <a href="./my_names.php">My Names</a>
+    </div>
+
+    <div class="col-sm">
+      <a href="./match_list.php">List of Matching Names</a>
+    </div>
+
+    <div class="col-sm">
+      <a href="./index.php">My Account/Signup</a>
+    </div>
+  </div>
+</div>
--- website/index.php
+++ website/index.php
@@ -19,26 +19,8 @@
 
 <body>
 
-<!-- Link Bar -->
-<div class="container">
-  <div class="row">
-    <div class="col-sm">
-      <a href="./show_names.php">Show Me Names!</a>
-    </div>
+<?php include("header.php"); ?>
 
-    <div class="col-sm">
-      <a href="./my_names.php">My Names</a>
-    </div>
-
-    <div class="col-sm">
-      <a href="./match_list.php">List of Matching Names</a>
-    </div>
-
-    <div class="col-sm">
-      <a href="./index.php">My Account/Signup</a>
-    </div>
-  </div>
-</div>
 
 <div class="container">
   <h2>Welcome to Baby Names Match!</h2>
