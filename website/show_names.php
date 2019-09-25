dec7f5b660bc3ad0a6ef4439488597048da50b4b
Ben Cerjan <bcerjan@gmail.com>
2019-09-25 11:58:07 -0500

    Initial commit of page for users to select names and approve / disapprove them

--- website/show_names.php
+++ website/show_names.php
@@ -4,36 +4,20 @@
 <meta charset="utf=8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Bootstrap Sourcing, CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-<!-- Bootstrap Sourcing, jQuery -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<?php include("./resources.php"); ?>
 
 
-
-<title>Edit Name Selections</title>
+<title>Pick Names!</title>
 </head>
 
 <?php
 // Region to set up PHP stuff
 
-//$selections = get_name_selections
-$selections = array(
-		1 => array(
-			'name' => 'Name 1',
-			'selected' => 'Yes'
-			),
-		2 => array(
-			'name' => 'Name 2',
-			'selected' => 'No'
-			)
-		);
+//$names = get_name_list();
+$names = array('Alice','Bob','Charlie');
 
-// Loop counter:
-$i = 0;
+// Prepare for passing to JS
+$names = json_encode($names);
 
 ?>
 
@@ -43,32 +27,86 @@ $i = 0;
 
 
 <div class="container">
-  <h2>Change what you think about a name</h2>
+  <h2>Upvote/Downvote Names</h2>
 
-<?php foreach($selections as $selection) { $i++?>
   <div class="row">
-    <div class="col-sm" name="name">
-      <?php echo(htmlspecialchars($selection['name'])); ?>
-    </div>
-    <div class="col-sm" name="selected">
-      <?php echo($selection['selected']); ?>
-    </div>
+    <!-- No button -->
     <div class="col-sm">
-      <button type="button" class="swap_btn">Swap</button>
+      <button type="button" class="select_btn" id="noName">&#10060</button>
     </div>
-  </div>
-<?php } ?>
+
+    <!-- Name -->
+    <div class="col-sm" id="nameText">
+
+    </div>
+
+    <!-- Yes button -->
+    <div class="col-sm">
+      <button type="button" class="select_btn" id="yesName">&#9989</button>
+    </div>
+
 </div>
 
 
 <!-- Custom JavaScript goes here -->
 <script>
-$('.swap_btn').click(function() {
-  // AJAX request to swap name choice in DB.
+var nameList = <?php echo($names) ?>;
 
-  console.log('Clicked!');
-
+$( document ).ready(function() {
+  // Set name to first available name:
+  $('#nameText').text(nameList[0]);
+  nameList.shift(); // Remove element we just used
+  console.log(nameList);
 });
+
+
+$('.select_btn').click(function() {
+  console.log(nameList.length);
+  // Get the name we're working on:
+  var name = $('#nameText').text().trim(); // trim() removes whitespace
+
+  // Then determine if we had 'yes' or 'no':
+  if( $(this).attr('id') == 'noName' ) {
+    // We don't like this name
+    nameRecord('no',name);
+  } else {
+    // We do!
+    nameRecord('yes',name);
+  }
+
+  // Finally, update with a new name:
+  $('#nameText').text(nameList[0]);
+  nameList.shift(); // Remove element we just used
+
+  if( nameList.length < 5) {
+    // Get more names!
+    //nameList.push(getNames()) // Add new names to end of array
+  }
+});
+
+// Function to record if we liked or disliked the name
+function nameRecord(status,oldName) {
+  var data = {action:'nameRecord', goodName:status,name:oldName};
+  // AJAX Request here
+/*$.ajax({
+    type: "POST",
+    url: "./endpoints/ajax_endpoint.php",
+    data: data
+  });*/
+}
+
+function getNames() {
+  var data = {action:'getNames'};
+  // AJAX Request here
+/*$.ajax({
+    type: "POST",
+    url: "./endpoints/ajax_endpoint.php",
+    data: data,
+    success: function() {
+      return result
+    }
+  });*/
+
 </script>
 
 </body>
