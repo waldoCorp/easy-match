a2dc654e6047a021bb3b3f1231e23616ff07a936
Ben Cerjan <bcerjan@gmail.com>
2019-10-04 18:04:47 -0500

    Initial commit of file containing variables for working with the database tables

---
+++ functions.php/table_variables.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * parametrize table names in case Ben goes crazy and wants to change up the database
+ * For the record, Lief thinks this is unnecessary
+ *
+ * But you never know...
+ */
+$names_table = 'names';
+$selections_table = 'selections';
+$partners_table = 'partners';
+$password_recovery_table = 'password_recovery';
+$users_table = 'users';
