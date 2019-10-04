1814ef42d4292b930757b63d0efcfea7a0ef06c7
Ben Cerjan <bcerjan@gmail.com>
2019-10-04 18:03:08 -0500

    Updated to use table variables source file and so it would actually hash the password (...oops)

--- functions.php/add_new_user.php
+++ functions.php/add_new_user.php
@@ -8,12 +8,11 @@
  * Example usage:
  * require_once '../add_new_user.php';
  *
- * add_new_user($table,$email,$passwd);
+ * add_new_user($email,$passwd);
  *
  *
  *
  * @author Ben Cerjan
- * @param string $table : Table to insert user in to
  * @param string $email : user's email address
  * @param string $passwd : hashed password
  *
@@ -21,7 +20,10 @@
 
 **/
 
-function add_new_user($table,$email,$pass) {
+function add_new_user($email,$pass) {
+	// Require table variables:
+	require '/srv/nameServer/functions.php/table_variables.php';
+
 	// Include database connection
 	require_once '/srv/nameServer/functions.php/db_connect.php';
 
@@ -33,18 +35,18 @@ function add_new_user($table,$email,$pass) {
 	$hashed_email = password_hash($email, PASSWORD_DEFAULT);
 
 	//$current_date = mktime();
-	$current_date = date('Y-m-d h:i:s')
+	$current_date = date('Y-m-d h:i:s');
 
 
 	try {
-		$sql = "INSERT INTO $table (uuid, email, create_date, last_login, password) VALUES (:uuid, :email, :create_date, :last_login, :password)
+		$sql = "INSERT INTO $users_table (uuid, email, create_date, last_login, password) VALUES (:uuid, :email, :create_date, :last_login, :pass)
 			ON CONFLICT DO NOTHING";
 		$stmt = $db->prepare($sql);
 		$stmt->bindValue(':uuid', $hashed_email);
 		$stmt->bindValue(':email', $email);
 		$stmt->bindValue(':create_date', $current_date);
 		$stmt->bindValue(':last_login', $current_date);
-		$stmt->bindValue(':pass', $pass);
+		$stmt->bindValue(':pass', $hashed_pass);
 		$success = $stmt->execute();
 	} catch(PDOException $e) {
 		die('ERROR: ' . $e->getMessage() . "\n");
