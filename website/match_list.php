ccb88ccf9d020b2a84c5a432f6773f33c6c47f78
Ben Cerjan <bcerjan@gmail.com>
2019-10-19 10:16:58 -0500

    Updated to work with testing users and actually show matching names

--- website/match_list.php
+++ website/match_list.php
@@ -17,9 +17,28 @@ require './login_script.php';
 
 <?php
 // Region to set up PHP stuff
+require_once '/srv/nameServer/functions.php/get_matching_names_list.php';
+require_once '/srv/nameServer/functions.php/get_uuid.php';
+//require_once '/srv/nameServer/functions.php/get_partners.php';
 
-//$names = get_matching_names_list($email1,$email2);
-$names = array('Alice','Bob','Charlie');
+$uuid = $_SESSION['uuid'];
+
+// Find existing partners:
+//$partners = get_partners($uuid);
+$partners = array('test@test1','test@test2','test@test3');
+
+
+$partner_email = $_SESSION['partner_email'];
+$partner_uuid = get_uuid($partner_email);
+
+// FOR TESTING
+//$partner_uuid = 'test1';
+//$partner_email = 'test1';
+
+
+$names = get_matching_names_list($uuid,$partner_uuid);
+
+//$names = array('Alice','Bob','Charlie');
 
 shuffle($names); // Randomizing seems like as good a call as any here
 // Maybe should sort alphabetically instead, but I'm not sure...
@@ -30,18 +49,76 @@ shuffle($names); // Randomizing seems like as good a call as any here
 
 <?php include("header.php"); ?>
 
+<div class="container">
+  <h2>Choose which partner to match names with</h2>
+
+  <select id="partner_select">
+    <option value="">Pick a Partner</option>
+    <?php foreach($partners as $partner) { ?>
+      <option value="<?php echo htmlspecialchars($partner); ?>"
+        <?php echo ($partner == $partner_email) ? 'selected' : ''; ?>>
+
+	<?php echo htmlspecialchars($partner); ?>
+      </option>
+    <?php } ?>
+  </select>
+</div>
+
+<br>
+
+
+
 
 <div class="container">
-  <h2>List of Names you and XXXX Agree On</h2>
+  <?php if( empty($partner_uuid) ) { ?>
+    <h2>No partner selected to match with</h2>
+  <?php } else { ?>
+    <h2>List of names you and <?php echo htmlspecialchars($partner_email); ?> agree on</h2>
 
-  <?php foreach($names as $name) { ?>
-  <div class="row">
-    <div class="col-sm">
-      <?php echo(htmlspecialchars($name)); ?>
-    </div>
-  </div>
+    <?php if( !is_null($names) ) {
+      foreach($names as $name) { ?>
+      <div class="row">
+        <div class="col-sm">
+          <?php echo(htmlspecialchars($name)); ?>
+        </div>
+      </div>
+      <?php } ?>
+    <?php } else { ?>
+      <div class="row">
+        <div class="col-sm">
+          No Matching Names!
+        </div>
+      </div>
+    <?php } ?>
   <?php } ?>
 </div>
+
+<script>
+$('#partner_select').change(function() {
+  var partner_email = $(this).val()
+  partnerSelect(partner_email);
+});
+
+function partnerSelect(email) {
+  // AJAX Request here
+  var data = {"action":'partnerSelect', "partner_email":email};
+
+  // AJAX Request here
+  $.ajax({
+    type: "POST",
+    dataType: "json",
+    url: "./endpoints/ajax_endpoint.php",
+    data: data,
+    success: function(data) {
+      location.reload()
+    }
+  });
+
+}
+
+
+</script>
+
 
 </body>
 
