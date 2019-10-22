b43c88a3a17c8f431c81b41be55d28143de166c9
Ben Cerjan <bcerjan@gmail.com>
2019-10-22 18:19:14 -0500

    Updated to reflect new DB standards and to track all relevant dates

--- functions.php/record_partner_choice.php
+++ functions.php/record_partner_choice.php
@@ -19,7 +19,7 @@
  *
 **/
 
-function record_partner_invitations($uuid,$partner_uuid,$confirm) {
+function record_partner_choice($uuid,$partner_uuid,$confirm) {
     // Require table variables:
     require '/srv/nameServer/functions.php/table_variables.php';
 
@@ -27,25 +27,49 @@ function record_partner_invitations($uuid,$partner_uuid,$confirm) {
     require_once '/srv/nameServer/functions.php/db_connect.php';
     $db = db_connect();
 
-    // MIGHT NEED TO DO THE REVERSE AS WELL FOR CONSISTENCY
-    // MAYBE TRIGGER????
-
     if( $confirm ) {
       // Accepted invitation:
-      $sql = "UPDATE $partners_table SET confirmed = true
-              WHERE uuid = :partner_uuid AND partner_uuid = :uuid";
+      // Create initial record:
+      $sql = "INSERT INTO $partners_table
+             (uuid, partner_uuid, proposer, pair_confirm_date, confirmed, pair_propose_date)
+             VALUES (:uuid, :partner_uuid, false, current_timestamp, true, current_timestamp)
+             ON CONFLICT DO NOTHING;";
 
       $stmt = $db->prepare($sql);
       $stmt->bindValue(':uuid',$uuid);
+      $stmt->bindValue(':partner_uuid',$partner_uuid);
+      $stmt->execute();
+
+      // Update first record with pair-date and confirmation:
+      $sql = "UPDATE $partners_table SET (pair_confirm_date, confirmed) =
+              (current_timestamp, true) WHERE uuid = :partner_uuid
+              AND partner_uuid = :uuid;";
+
+      $stmt = $db->prepare($sql);
+      $stmt->bindValue(':uuid',$uuid);
+      $stmt->bindValue(':partner_uuid',$partner_uuid);
+      $stmt->execute();
+
+      // Update second record to have original proposal time:
+      $sql = "UPDATE $partners_table SET pair_propose_date =
+                (SELECT pair_propose_date from $partners_table
+                 WHERE uuid = :partner_uuid AND partner_uuid = :uuid)
+              WHERE uuid = :uuid AND partner_uuid = :partner_uuid;";
+
+      $stmt = $db->prepare($sql);
+      $stmt->bindValue(':uuid',$uuid);
+      $stmt->bindValue(':partner_uuid',$partner_uuid);
       $stmt->execute();
 
     } else {
+
       // Rejected invitation:
       $sql = "DELETE FROM $partners_table
               WHERE uuid = :partner_uuid AND partner_uuid = :uuid";
 
       $stmt = $db->prepare($sql);
       $stmt->bindValue(':uuid',$uuid);
+      $stmt->bindValue(':partner_uuid',$partner_uuid);
       $stmt->execute();
-
+    }
 }
