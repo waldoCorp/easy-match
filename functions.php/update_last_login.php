80d1b681799e5973b91fe0784336f695f559828a
Ben Cerjan <bcerjan@gmail.com>
2019-10-28 17:40:47 -0500

    Initial commit of function to track a user's last login time

---
+++ functions.php/update_last_login.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Function to track when a user last logged in:
+ *
+ * Example usage:
+ * require_once '../update_last_login.php';
+ *
+ * update_last_login($uuid);
+ *
+ *
+ *
+ * @author Ben Cerjan
+ * @param string $uuid: user's UUID
+ *
+ * no return
+**/
+
+function update_last_login($uuid) {
+	// Require table variables:
+	require '/srv/nameServer/functions.php/table_variables.php';
+
+	// Include database connection
+	require_once '/srv/nameServer/functions.php/db_connect.php';
+
+	// Connect to db
+	$db = db_connect();
+
+	try {
+		$sql = "UPDATE $users_table SET last_login = current_timestamp
+			WHERE uuid = :uuid";
+		$stmt = $db->prepare($sql);
+		$stmt->bindValue(':uuid', $uuid);
+		$stmt->execute();
+	} catch(PDOException $e) {
+		die('ERROR: ' . $e->getMessage() . "\n");
+	}
+
+}
