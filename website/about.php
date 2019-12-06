d916078a6d7f5ab8e85358e09fb2d19ec2e37526
Lief <williamlief@gmail.com>
2019-12-06 01:04:59 -0600

    reworked about page relates #16 - added privacy policy relates #26 - added Using Name Match - added Waldo Corp - added contact us, but still needs email

--- website/about.php
+++ website/about.php
@@ -19,17 +19,42 @@
   <h2>About Baby Name Match</h2>
   <p>This website is designed to allow users to pair with one another
      to find names that they both find palatable for any potential young
-     individuals in their lives (or cats, fish, iguanas, or whatever -- we're
+     individuals in their lives (or cats, fish, iguanas, or whatever - we're
      not here to judge). It is hosted as a service of WaldoCorp, which is just
      a fancy way of saying the Ben and Lief made it and they hope that it is
      useful for you. </p>
 
-  <h2>About the Data</h2>
+  <h3>Using Name Match </h3>
+  <ul>
+    <li> Select names you like on 'Show Me Names' </li>
+    <li> Invite your friends through the 'Manage Partners Page' </li>
+    <li> View the name matches on 'List of Matching Names' </li>
+    <li> You can always change your mind about a name on 'My Names'  </li>
+  </ul>
+
+  <h3>About the Data</h3>
    <p>The set of names that are used on this site are drawn from
       the <a href="https://www.ssa.gov/OACT/babynames/limits.html"> US Social Security database</a> . That means that any name that was given
-      to at least 5 newborns in a given year since 1880
+      to at least 5 newborns in a year since 1880
       is inside the database (somewhere, there are a lot of dusty corners around
       here). </p>
+
+   <h2>WaldoCorp</h2>
+   <p> WaldoCorp is not (yet) a real corporation, but its the name Ben and Lief picked  
+       as an umbrella for various projects. 
+       Its named for Waldo, an adorable cat who started life in a college dorm room, 
+       and now is the Queen of Ben's apartment. Baby Name Match is our first product, and we
+       hope to create more cool things in the future.</p>
+
+   <h2>Privacy Policy</h2>
+   <p> We will never sell your data. We will never share your email address. We may share reports
+       on interesting (or boring) things we find in the data, such reports will be presented in aggregate 
+       form and will not identify individuals. We may share the anonymized data on name selections and 
+       partner matches with other people who are interested in it. </p>
+
+   <h2>Contact Us!</h2>
+   <p> Love baby name match? Hate it? Got great ideas for how to make it better or are you just finding tons of bugs? 
+   Let us know at ... email address ... </p>
 </div>
 
 </body>
