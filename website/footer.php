74fb3d55e35875da38ee1676e8f99f6afcaeff0e
Ben Cerjan <bcerjan@gmail.com>
2019-12-07 11:33:32 -0600

    Updated to include JS to automatically apply the .active class to the current page to make it more clear what page a user is on. It is still sort of hard to tell (at least for me), but in theory it is easy to fix now if others do not like it.

--- website/footer.php
+++ website/footer.php
@@ -3,3 +3,13 @@
     Find a bug? Want a new feature? Like the site? <a href='mailto:ben@waldocorp.com,lief@waldocorp.com'>Drop us a line!</a>
   </span>
 </div>
+
+<!-- Script to bold current page in navbar -->
+<script>
+$(document).ready(function() {
+  $('li.active').removeClass('active');
+  const url = window.location.pathname;
+  const filename = url.substring(url.lastIndexOf('/')+1);
+  $('a[href="' + filename + '"]').closest('li').addClass('active');
+});
+</script>
