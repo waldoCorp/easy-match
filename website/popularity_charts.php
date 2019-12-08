ea5fde9c2f2fe4d4afb8f5d4f1bf2de4f94ff357
Ben Cerjan <bcerjan@gmail.com>
2019-12-08 13:10:23 -0600

    Initial commit of embedded shiny app

---
+++ website/popularity_charts.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf=8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php include("./resources.php"); ?>
+
+<title>Chart Name Popularity</title>
+
+
+</head>
+<body>
+<?php include("header.php"); ?>
+
+<div class="container">
+<iframe id="example1" src="./shiny/popularityCharts" style="border: none; width: 100%; height: 850px" frameborder="0"></iframe>
+</div>
+
+
+<?php include("footer.php"); ?>
+</body>
+</html>
