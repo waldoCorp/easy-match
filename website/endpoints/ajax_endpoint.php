24e42058e8f5f490776ad5ec205cd834555a18e4
Ben Cerjan <bcerjan@gmail.com>
2019-12-12 18:06:42 -0600

    Updated to only process invites if provided a valid email address and to allow for username updates

--- website/endpoints/ajax_endpoint.php
+++ website/endpoints/ajax_endpoint.php
@@ -18,6 +18,7 @@ if (is_ajax()) {
 			case "partnerSelect": partner_select(); break;
 			case "partnerResponse": partner_response(); break;
 			case "email_check": unique_email(); break;
+			case "unameUpdate": uname_update(); break;
 			case "send_password_token": send_password_token(); break;
 			case "preferencesRecord": pref_record(); break;
 			case "deleteAccount": delete_acc(); break;
@@ -50,15 +51,23 @@ function name_record() {
 
 }
 
+function uname_update() {
+	global $function_path;
+	require_once $function_path . 'update_username.php';
+        $uuid = $_SESSION['uuid'];
+	$uname = $_POST['uname'];
+	update_username($uuid,$uname);
+}
+
 function invite_friend() {
 	global $function_path;
         require_once $function_path . 'invite_partner.php';
 	$new_email = $_POST['new_email'];
 	$orig_uuid = $_SESSION['uuid']; // Maybe use orig_email instead?
 
-	//if (filter_var($return["email"], FILTER_VALIDATE_EMAIL)) {
+	if (filter_var($new_email, FILTER_VALIDATE_EMAIL)) {
 		invite_partner($new_email,$orig_uuid);
-	//}
+	}
 }
 
 function partner_select() {
