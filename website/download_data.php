bf83af8d9a572d57b9ec2145b472b67ba602198e
Lief <williamlief@gmail.com>
2019-12-15 23:31:07 -0800

    Shiny page to allow user to download data - sql/r hybrid scripts to pull data from server - shiny page to display and download - php wrapper page - script to hold database connections, put one level up, should be shared across dashboards

---
+++ website/download_data.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf=8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php include("./resources.php"); ?>
+
+<title>Data Download</title>
+
+
+</head>
+<body>
+<?php include("header.php"); ?>
+
+<main role="main">
+<div class="container">
+<iframe id="example1" src="./shiny/downloadData" style="border: none; width: 100%; height: 850px" frameborder="0"></iframe>
+</div>
+</main>
+
+<?php include("footer.php"); ?>
+</body>
+</html>
---
+++ website/shiny/downloadData/app.R
@@ -0,0 +1,73 @@
+library(shiny)
+library(DBI)
+library(RPostgres)
+
+# Define UI for data download app ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Downloading Data"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Choose dataset ----
+      selectInput("dataset", "Choose a dataset:",
+                  choices = c(
+                    "Account Data", 
+                    "Friend List", 
+                    "Name Selections", 
+                    "Name Matches")),
+      
+      # Button
+      downloadButton("downloadData", "Download")
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      tableOutput("table")
+      
+    )
+    
+  )
+)
+
+
+server <- function(input, output) {
+  
+  source("../conn.R")
+  source("sql.R")
+  
+  # Reactive value for selected dataset ----
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "Account Data" = dbGetQuery(con, get_user(uuid)),
+           "Friend List"  = dbGetQuery(con, get_partners(uuid)),
+           "Name Selections"   = dbGetQuery(con, get_selections(uuid)), 
+           "Name Matches" = dbGetQuery(con, get_matches(uuid)))
+  })
+  
+  # Table of selected dataset ----
+  output$table <- renderTable({
+    datasetInput()
+  })
+  
+  # Downloadable csv of selected dataset ----
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(datasetInput(), file, row.names = FALSE)
+    }
+  )
+  
+}
+
+# Create Shiny app ----
+shinyApp(ui, server)
