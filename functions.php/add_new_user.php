cba25296a39fe5af98cb1c1409e1b71bcd5c256d
Ben Cerjan <bcerjan@gmail.com>
2019-12-19 15:09:04 -0600

    Updated new user function to use a real UUIDv4 instead of a hased email as a uuid. Also initial commit of function to generate UUIDv4's.

--- functions.php/add_new_user.php
+++ functions.php/add_new_user.php
@@ -26,17 +26,19 @@ function add_new_user($email,$pass,$uname) {
 	require __DIR__ . '/table_variables.php';
 
 	// Include database connection
-	require_once __DIR__ . '/db_connect.php';
-
+u
 	// Connect to db
 	$db = db_connect();
 
 	// make email lower case
         $email = strtolower($email);
 
-	// hash inputs
+	// hash input password
 	$hashed_pass  = password_hash($pass, PASSWORD_DEFAULT);
-	$hashed_email = password_hash($email, PASSWORD_DEFAULT);
+
+	// Generate UUID:
+	require_once __DIR__ . '/generate_uuidv4.php';
+	$uuid = generate_uuid();
 
 	//$current_date = mktime();
 	$current_date = date('Y-m-d h:i:s');
@@ -47,7 +49,7 @@ function add_new_user($email,$pass,$uname) {
                         VALUES (:uuid, :email, :create_date, :last_login, :pass, :uname)
 			ON CONFLICT DO NOTHING";
 		$stmt = $db->prepare($sql);
-		$stmt->bindValue(':uuid', $hashed_email);
+		$stmt->bindValue(':uuid', $uuid);
 		$stmt->bindValue(':email', $email);
 		$stmt->bindValue(':create_date', $current_date);
 		$stmt->bindValue(':last_login', $current_date);
