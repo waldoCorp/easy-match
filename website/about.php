29feca041431246a773ccca06bee0f5410968023
Lief Esbenshade <williamlief@gmail.com>
2020-03-09 22:35:00 -0700

    Ui stuff (#120)
    * removed popularity charts from nav bar (relates #113)
    
    * styled "waldoCorp" consistently
    fix #105
    
    * added filter definitions to about, fix #115

--- website/about.php
+++ website/about.php
@@ -17,10 +17,10 @@
 <main role="main">
 <div class="container">
   <h2>About Easy Match</h2>
-  <p>Have someone (human, cat, iguana, we don't judge) coming into your life that needs a name? 
+  <p>Have someone (human, cat, iguana, we don't judge) coming into your life that needs a name?
      This website is designed to allow users to pair with one another
-     to find names that they both love. Sign up with as many partners or friends as you like, 
-     then start picking the names that speak to you. 'My Matches!' will update you 
+     to find names that they both love. Sign up with as many partners or friends as you like,
+     then start picking the names that speak to you. 'My Matches!' will update you
      whenever you and your partner both like the same name. Happy name matching!</p>
 
   <h2>About the Data</h2>
@@ -30,27 +30,45 @@
       is inside the database (somewhere, there are a lot of dusty corners around
       here). </p>
 
+   <h3> Filter Definitions</h3>
+    <p>We give you the ability to apply filters to the names you see to help you
+      find the names you like more quickly.
+      <ul>
+        <li> Gender is defined using the data provided by
+          the social security administration. We use the last ten years of data to determine
+          name/gender associations. "Traditionally Boys" and "Traditionally Girls"
+          names are names where more than 95% of the babys born are of that gender.
+          We define "gender neutral names" as names where between 20% and 80% of the
+          babies given that name are identified as male.</li>
+        <li> Popularity is determined using the names average annual rank. By default,
+          we show you names that are in the top 500 most popular."Popular names only"
+          will only show names that are in the top 250. "Unusual names only" will show
+          you names that are not in the top 500.  </li>
+      </ul>
+    </p>
+
+
   <h2>About Privacy</h2>
-  <p>See our <a href='privacy.php'>official privacy policy</a> here. 
-     The short version is: we will never sell your data. We will be analyzing your usage 
-     of the site to try and make it better, and to see if we can learn anything cool 
-     about how people pick baby names. To that end, we may also share anonymized selection 
+  <p>See our <a href='privacy.php'>official privacy policy</a> here.
+     The short version is: we will never sell your data. We will be analyzing your usage
+     of the site to try and make it better, and to see if we can learn anything cool
+     about how people pick baby names. To that end, we may also share anonymized selection
      and partnering data with other organizations.
-     You can delete your  account at any time, at which point any identifiable data about you 
-     will be deleted. We will also delete your partner data and selection data from our servers, 
+     You can delete your  account at any time, at which point any identifiable data about you
+     will be deleted. We will also delete your partner data and selection data from our servers,
      but your anonymized data will remain part of any datasets we have shared (as we no longer
-     control it, we cannot guarantee deletion). You can request a copy of all data we have stored 
+     control it, we cannot guarantee deletion). You can request a copy of all data we have stored
      about you at any time. You can opt-out of data sharing, view your data, and delete your account <a href='account.php'> here</a>.</p>
 
-  <h2>WaldoCorp</h2>
-  <p> Easy Match is hosted as a service of WaldoCorp, which is just
+  <h2>waldoCorp</h2>
+  <p> Easy Match is hosted as a service of waldoCorp, which is just
      a fancy way of saying that <a href='https://ben.waldocorp.com'>Ben</a>
      and <a href='https://liefesbenshade.com'>Lief</a> made it and we hope
      that it is useful for you.
 
-     WaldoCorp is not (yet) a registered corporation, but its the name Ben and Lief picked  
-     as an umbrella for various projects. 
-     Its named for Waldo, an adorable cat who started life in a college dorm room, 
+     waldoCorp is not (yet) a registered corporation, but its the name Ben and Lief picked
+     as an umbrella for various projects.
+     Its named for Waldo, an adorable cat who started life in a college dorm room,
      and now is the Queen of Ben's apartment. Easy Match is our first product, and we
      hope to create more cool things in the future.</p>
 
