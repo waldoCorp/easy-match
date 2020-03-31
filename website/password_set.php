4c3a33aeae00712a774dde9584fdf5af2b96c977
Ben Cerjan <bcerjan@gmail.com>
2020-03-31 13:41:50 -0500

    Updated to include license header

--- website/password_set.php
+++ website/password_set.php
@@ -1,4 +1,22 @@
 <!DOCTYPE html>
+<!--
+ -    Copyright (c) 2020 Ben Cerjan, Lef Esbenshade
+ -
+ -    This file is part of Easy Match.
+ -
+ -    Easy Match is free software: you can redistribute it and/or modify
+ -    it under the terms of the GNU Affero General Public License as published by
+ -    the Free Software Foundation, either version 3 of the License, or
+ -    (at your option) any later version.
+ -
+ -    Easy Match is distributed in the hope that it will be useful,
+ -    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ -    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ -    GNU Affero General Public License for more details.
+ -
+ -    You should have received a copy of the GNU Affero General Public License
+ -    along with Easy Match.  If not, see <https://www.gnu.org/licenses/>.
+-->
 <html lang="en">
 <head>
 <meta charset="utf=8">
--- website/privacy.php
+++ website/privacy.php
@@ -1,4 +1,22 @@
 <!DOCTYPE html>
+<!--
+ -    Copyright (c) 2020 Ben Cerjan, Lef Esbenshade
+ -
+ -    This file is part of Easy Match.
+ -
+ -    Easy Match is free software: you can redistribute it and/or modify
+ -    it under the terms of the GNU Affero General Public License as published by
+ -    the Free Software Foundation, either version 3 of the License, or
+ -    (at your option) any later version.
+ -
+ -    Easy Match is distributed in the hope that it will be useful,
+ -    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ -    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ -    GNU Affero General Public License for more details.
+ -
+ -    You should have received a copy of the GNU Affero General Public License
+ -    along with Easy Match.  If not, see <https://www.gnu.org/licenses/>.
+-->
 <html lang="en">
 <head>
 <meta charset="utf=8">
@@ -18,17 +36,17 @@
 <div class="container">
   <h2>WaldoCorp Privacy Policy</h2>
   <p> Ben and Lief like making things, and now we’re sharing them with you.
-      Our first product, Easy Match, helps you find names that you, and your partners, love. 
+      Our first product, Easy Match, helps you find names that you, and your partners, love.
       To do that, we store the minimum amount of data necessary to make the website run.
       This privacy policy was updated on December 27, 2019 </p>
 
   <h3> How We Collect Your data </h3>
   <p>  All the data we collect is explicitly provided by you when you fill out the sign up form,
-       choose names you like, invite friends to partner with, or fill out a survey.  
+       choose names you like, invite friends to partner with, or fill out a survey.
        We also log the time at which these events occur. </p>
 
   <h3> What Data We Collect </h3>
-  <ul> 
+  <ul>
      <li> When you sign up we will ask for your email address and a user name. We require a working
        email address to use our site. This allows you to set and reset your password, and to partner
         with other users. It allows us to communicate with you about your account. </li>
@@ -44,36 +62,36 @@
           participation.  </li>
   </ul>
 
-  <h3> What We Can Do With Your Data </h3> 
-  <p> We will never share any email addresses or user names except as required by law (see <b>Legal Requests for Data</b>). 
-      We reserve the right to share anonymized data of name ratings and partnerships. We may share 
+  <h3> What We Can Do With Your Data </h3>
+  <p> We will never share any email addresses or user names except as required by law (see <b>Legal Requests for Data</b>).
+      We reserve the right to share anonymized data of name ratings and partnerships. We may share
       this anonymized data to improve our product or to further academic research into matching
       behavior or baby naming trends. While the data we may share will be fully anonymized, it is
-      possible that other entities can correlate the data we release with other available data. 
-      If you are rating names on other web services, or other information about names you like are 
+      possible that other entities can correlate the data we release with other available data.
+      If you are rating names on other web services, or other information about names you like are
       publicly available, there is a risk that your records could be re-identified. This is not a
       likely outcome, and the terms of use for any partners we may share the data with require that
       they explicitly agree that they will not attempt to de-anonymize the data. You can opt-out of this
       data sharing at any time on the <a href='account.php'> account page</a>. </p>
 
   <h3> Data Retention </h3>
-  <p> We will retain your data indefinitely, unless you decide to delete your account. If you 
+  <p> We will retain your data indefinitely, unless you decide to delete your account. If you
       delete your account, we will immediately remove all of your data from our servers. </p>
 
   <h3> View And Delete Your Data </h3>
   <p> You can view and download all data we have stored about you at any time.
-      You can also delete your account and all data associated with from our servers at any time. 
+      You can also delete your account and all data associated with from our servers at any time.
       These actions can be taken on the <a href='account.php'> account page</a>.
       Please note, if we have shared your anonymized data with third parties
-      we will not be able to delete that anonymized data, however your data will not be part of 
-      any future anonymized datasets that may be shared. We do not support data 
+      we will not be able to delete that anonymized data, however your data will not be part of
+      any future anonymized datasets that may be shared. We do not support data
       transfer to or from any other websites. </p>
 
   <h3> California Do Not Track Disclosure </h3>
   <p> We do not acknowledge your browsers do not track setting. See the <b>Cookies</b> section for more information </p>
 
   <h3> Cookies and Tracking </h3>
-  <ul> 
+  <ul>
     <li> We use a session cookie to store your log in credentials. This cookie is only used to enable your access to this website.  </li>
     <li> We track your logged in use of our site across sessions. We do not track any use of our site while you are not logged in.</li>
     <li> We do not use any third party analytics, we do all of our site analytics ourselves. </li>
@@ -83,7 +101,7 @@
 
   <h3> Legal Requests For Data </h3>
   <p> If served with a lawful warrant for user data we will comply and turn over all requested data.  </p>
-  
+
   <h3> Automated Decision Making and Machine Learning </h3>
   <p> Our site currently randomly selects names to show you based on the filters you apply, the names
       your partners like, and simple popularity rules. In the future we intend to improve our site by
@@ -93,7 +111,7 @@
       experience on the site. </p>
   </p>
 
-  <h3> International Data Use </h3> 
+  <h3> International Data Use </h3>
   <p> Our servers are located in the U.S.A.  All data is stored locally on servers under Ben’s physical control. </p>
 
   <h3> Children's Data </h3>
