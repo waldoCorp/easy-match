a69f03a59a0e52a19ed2b646ad76dae3ccc0a18d
Lief <williamlief@gmail.com>
2020-03-31 22:06:47 -0500

    added license info to R files

--- website/shiny/analyticsDB/app.R
+++ website/shiny/analyticsDB/app.R
@@ -1,3 +1,21 @@
+#'  Copyright (c) 2020 Lief Esbenshade
+#'
+#'    This file is part of Easy Match.
+#'
+#'    Easy Match is free software: you can redistribute it and/or modify
+#'    it under the terms of the GNU Affero General Public License as published by
+#'    the Free Software Foundation, either version 3 of the License, or
+#'    (at your option) any later version.
+#'
+#'    Easy Match is distributed in the hope that it will be useful,
+#'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#'    GNU Affero General Public License for more details.
+#'
+#'    You should have received a copy of the GNU Affero General Public License
+#'    along with Easy Match.  If not, see <https://www.gnu.org/licenses/>.
+
+
 library(shiny)
 library(tidyverse)
 library(DBI)
